7c18b8ca4eaa2b723ee0e50ce37a303ff0b8dbe2
radostboyadzhieva <radb@itu.dk>
2025-11-25 14:15:46 +0100

    Added FFNN from scratch

---
+++ functions.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+def clean_data(file):
+    df = pd.read_csv(file)
+
+    Y = df['ClaimNb']
+
+    df['VehIsregular'] = (df['VehGas'] == 'Regular').astype(int)
+
+    df = df.drop(columns= ['VehGas', 'IDpol', 'ClaimNb'])
+
+    def letter_to_index(letter):
+        return ord(letter.upper()) - ord('A')
+
+    df['Area'] = df['Area'].apply(letter_to_index)
+
+    df = pd.get_dummies(df)
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(df)
+   
+    return X, Y
+
+def split_data(X, Y, test_size):
+    return train_test_split(X, Y, test_size=test_size, random_state=1)
